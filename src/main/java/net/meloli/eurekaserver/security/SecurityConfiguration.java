8e31142439a672f4b2e705fcfeed7fce97da8463
LiRETRO <hash@meloli.net>
2018-12-10 18:34:22 +0800

    更改密码加密方式

--- src/main/java/net/meloli/eurekaserver/security/SecurityConfiguration.java
+++ src/main/java/net/meloli/eurekaserver/security/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -23,9 +24,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
+            .passwordEncoder(new BCryptPasswordEncoder())
             .withUser(username)
-            .password(password)
-            .roles("USER");
-        super.configure(auth);
+            .password(new BCryptPasswordEncoder().encode(password))
+            .roles("ADMIN");
     }
 }
